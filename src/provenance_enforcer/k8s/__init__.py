989d5792e5709f93c7106fa69bf06f6c37f0ecee
Stan Sabin <sabinstan19@gmail.com>
2026-04-17 01:03:08 +0300

    fix: fsnotify watcher: too many open files

--- src/provenance_enforcer/k8s/__init__.py
+++ src/provenance_enforcer/k8s/__init__.py
@@ -1,13 +1,12 @@
 """kubernetes helpers."""
 
-from .policies import get_matching_policy, labels_match, policy_targets_zta
+from .policies import get_matching_policy, policy_targets_zta
 from .status import default_provenance_state, patch_status, set_failure_status
 
 
 __all__ = [
 	"default_provenance_state",
 	"get_matching_policy",
-	"labels_match",
 	"patch_status",
 	"policy_targets_zta",
 	"set_failure_status",
